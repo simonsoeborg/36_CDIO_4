211a2b94eac432d32da049997c8f9c6fd06e3632
Simon <s173255@student.dtu.dk>
2020-01-06 12:15:10 +0100

    Initial commit

---
+++ src/main/java/View/MatadorSpil.java
@@ -0,0 +1,11 @@
+package View;
+
+import gui_fields.GUI_Board;
+import gui_fields.GUI_Field;
+import gui_main.GUI;
+
+public class MatadorSpil {
+    public static void main(String[] args) {
+        GUI gui = new GUI();
+    }
+}
