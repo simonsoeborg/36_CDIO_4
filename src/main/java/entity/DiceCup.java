a2e53c1752511743dda0c3e70f2f19ff4bec086d
Kristoffer <ktis10@hotmail.com>
2020-01-09 11:16:56 +0100

    Die klasse updateret metoder

--- src/main/java/entity/DiceCup.java
+++ src/main/java/entity/DiceCup.java
@@ -2,4 +2,14 @@ package entity;
 
 public class DiceCup {
 
+    protected int dice;
+
+public DiceCup (int die) {this.dice = dice;}
+
+    /* Terningekastet defineres. dvs. hvis man skriver d1.roll svarer det til terning 1 kastes.
+    Koden skal retunere en værdi fra kastet 1 til 6 */
+    public int roll() {
+       return value;
+    }
+
 }
--- src/main/java/entity/Die.java
+++ src/main/java/entity/Die.java
@@ -6,15 +6,7 @@ public class Die {
 
     /* Værdien af terningerne skal være et tal fra 1 til 6.
      * Vi siger +1 af (math.random() * MAX) som ellers vil være fra 0-5 */
-    public Die() {
-        this.faceValue = (int) (Math.random() * MAX) + 1;
-    }
-
-    /* Terningekastet defineres. dvs. hvis man skriver d1.roll svarer det til terning 1 kastes.
-    Koden skal retunere en værdi fra kastet 1 til 6 */
-    public int roll() {
-        this.faceValue = (int) (Math.random() * MAX) + 1;
-        return faceValue;
+    public Die() { this.faceValue = (int) (Math.random() * MAX) + 1;
     }
 
     public int getFaceValue() {
@@ -24,6 +16,4 @@ public class Die {
     public void setFaceValue(int faceValue) {
         this.faceValue = faceValue;
     }
-
-
 }
