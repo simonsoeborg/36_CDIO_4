8f0478f8c48549edb49ff7c2cd0ad357ba8f9aa1
David Mikkelsen <Davidmikkelsen@live.dk>
2020-01-10 12:22:06 +0100

    Har oprettet testen "PlayerMoveBoard" under test-mappen. Den er kun påbegyndt, da der ventes på logik mellem terning og board

---
+++ src/main/java/Test/PlayerMoveBoard.java
@@ -0,0 +1,27 @@
+package Test;
+
+import entity.Player;
+
+import java.awt.*;
+
+/**
+ * Tests' the moveability of a player on the board
+ * @author David
+ * @version 1.0
+ */
+public class PlayerMoveBoard {
+
+    public static void main(String[] args) {
+
+        Player spiller = new Player("Karsten", Color.black);
+        //todo:  Venter på at logikken bag terning og felter er udviklet
+
+
+
+    }
+
+
+
+
+
+}
