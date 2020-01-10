d819c3faa791ec1c5ed5d507af67510e5c382d75
Simon <fridolf.lindhard@gmail.com>
2020-01-10 11:12:34 +0100

    start på fieldlist

--- src/main/java/entity/FieldList.java
+++ src/main/java/entity/FieldList.java
@@ -1,4 +1,22 @@
 package entity;
 
+
+import fields.Field;
+
 public class FieldList {
+    private Field[] fields = new Field[40];
+
+    public FieldList() {
+        fields[0] = new Field ("Start");
+        fields[1] = new Field ("Rødovrevej");
+        fields[2] = new Field ("Prøv lykken");
+        fields[3] = new Field ("Hvidovrevej");
+        fields[4] = new Field ("Indkomstskat");
+        fields[5] = new Field ("Øresund");
+        fields[6] = new Field ("Roskildevej");
+        fields[7] = new Field ("Prøv lykken");
+        fields[8] = new Field ("Vallby Langade");
+        fields[9] = new Field ("Allégade");
+        fields[10] = new Field ("Fængsel");
+        fields[11] = new Field ("Frederiksberg Allé");
 }
