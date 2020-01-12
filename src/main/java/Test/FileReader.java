559c15015d6e15f2df2991e393d3f0133aaa9085
David Mikkelsen <Davidmikkelsen@live.dk>
2020-01-12 15:13:03 +0100

    Lavet en ny klasse Filereader og placeret denne i test-mappen. Klassen indeholder noget fra et andet projekt og skal lige finpusses

---
+++ src/main/java/Test/FileReader.java
@@ -0,0 +1,55 @@
+package Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class FileReader {
+
+    private String file1, file2, file3;
+
+    /**
+     * @param file1 : First file to read from
+     * @param file2 : Second file to read from
+     * @param file3 : Third file to read from
+     */
+    public FileReader(String file1, String file2, String file3){
+
+        this.file1 = file1;
+        this.file2 = file2;
+        this.file3 = file3;
+    }
+
+    /**
+     * @param line: The specific line to read
+     * @return the content of the line
+     */
+    public String read(int filenumber, int line){
+
+        String contentOfLine = "";
+        String file="";
+
+        //todo --> Skal finde en måde at skifte til den valgte fil ( tror en switch-case er fint nok )
+        switch (filenumber){
+            case 1: file = file1;
+                break;
+            case 2: file = file2;
+                break;
+            case 3: file = file3;
+                break;
+            default: System.out.println("Filen findes ikke");
+        }
+
+        try{
+            Stream<String> text = Files.lines(Paths.get(file));
+            contentOfLine = text.skip(line-1).findFirst().get();
+
+        }catch (IOException e){
+
+            System.out.println("Filen kunne ikke findes");
+        }
+
+        return contentOfLine;
+    }
+}
