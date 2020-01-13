c7bd1281d70c5ff172870f7ff24acdbede1ee0e9
David Mikkelsen <Davidmikkelsen@live.dk>
2020-01-13 14:00:42 +0100

    Oprette logkcontroller i gamecontrolleren og har begyndt en metode "checkFieldType" i logikcontrolleren

--- src/main/java/Control/LogicController.java
+++ src/main/java/Control/LogicController.java
@@ -2,6 +2,7 @@ package Control;
 
 import Entity.DiceCup;
 import Entity.FieldCreator;
+import Entity.Fields.Field;
 import Entity.Fields.Ownable;
 import Entity.Player;
 
@@ -10,7 +11,6 @@ public class LogicController {
 
     DiceCup d = new DiceCup();
 
-    //todo MovePlayer skal testes.
 
     /**
      * @param p  : The player who's turn it is.
@@ -26,6 +26,48 @@ public class LogicController {
         // todo Add value er endnu ikke relevant iforhold til vores test.
         if (actual < previous)
             p.addMoney(4000);
+    }
+
+    public Field checkFieldType(int fieldIndex){
+
+        switch (fieldIndex){
+            // Dette er ejendomsfelter
+            case 1: case 3: case 6: case 8: case 9: case 11: case 13: case 14: case 16: case 18: case 19:
+            case 21: case 23: case 24: case 26: case 27: case 29: case 31: case 32: case 34: case 37: case 39: kjslafjalfk;
+            break;
+
+            // Prøv lykken felter
+            case 2: case 7: case 17: case 22: case 33: case 36: jfajfjalj;
+            break;
+
+            // færge felter
+            case 5: case 15: case 25: case 35: khfakfhkh;
+            break;
+
+            // brygerier
+            case 12: case 28: hafkfhkash;
+            break;
+
+            //Indkomstskat
+            case 4: jlajfljadslj;
+            break;
+
+            //Statsskat
+            case 38: fjaljfladjs;
+            break;
+
+            //start, parkering og besøg i fængsel felter
+            case 0: case 10: case 20: kjahfkjhfk;
+            break;
+
+            // gå i fængsel
+            case 30: ljfalfj;
+            break;
+
+
+        }
+
+
     }
 
 
