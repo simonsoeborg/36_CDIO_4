8f2032da3f775c780f79966dc5c75854b0994e31
Kristoffer <ktis10@hotmail.com>
2020-01-13 10:53:52 +0100

    removed spaces in javadoks

--- src/main/java/Control/LogicController.java
+++ src/main/java/Control/LogicController.java
@@ -2,6 +2,7 @@ package Control;
 
 import Entity.DiceCup;
 import Entity.FieldCreator;
+import Entity.Fields.Ownable;
 import Entity.Player;
 
 
@@ -12,18 +13,18 @@ public class LogicController {
     //todo MovePlayer skal testes.
 
     /**
-     * @param p : The player who's turn it is.
+     * @param p  : The player who's turn it is.
      * @param fl : Our fieldList.
-     *  The method is changing the players FieldIndex, and therefore its location.
+     *           The method is changing the players FieldIndex, and therefore its location.
      */
-    public void movePlayer( Player p, FieldCreator fl) {
+    public void movePlayer(Player p, FieldCreator fl) {
         d.roll();
         int previous = p.getFieldIndex();
-        p.setFieldIndex((p.getFieldIndex()+d.faceValues())%fl.getSize());
+        p.setFieldIndex((p.getFieldIndex() + d.faceValues()) % fl.getSize());
         int actual = p.getFieldIndex();
 
         // todo Add value er endnu ikke relevant iforhold til vores test.
-        if (actual<previous)
+        if (actual < previous)
             p.addMoney(4000);
     }
 }
