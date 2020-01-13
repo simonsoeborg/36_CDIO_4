1b89d996a143c25d5525560f0c2828567a882885
Simon <fridolf.lindhard@gmail.com>
2020-01-13 10:42:07 +0100

    BreweryRentFactor insat

--- src/main/java/Entity/Fields/Brewery.java
+++ src/main/java/Entity/Fields/Brewery.java
@@ -3,6 +3,11 @@ package Entity.Fields;
 import java.awt.*;
 
 public class Brewery extends Ownable {
+    /**
+     * BreweryRentFactor is the factor which must be multipled with amount of brewerys and facevalue (in logic)
+     */
+    private int BreweryRentFactor = 100;
+
     public Brewery(String fieldName, int propertyPrice, int propertyRent, int propertyPledgePrice, int ownerID, Color propertyColor) {
         super(fieldName, propertyPrice, propertyRent, propertyPledgePrice, ownerID, propertyColor);
     }
