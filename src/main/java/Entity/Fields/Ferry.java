4574067287a6572b23f0984912ada1e1b9cbac8b
Simon <fridolf.lindhard@gmail.com>
2020-01-13 10:36:44 +0100

    FerryRentFactor insat

--- src/main/java/Entity/Fields/Ferry.java
+++ src/main/java/Entity/Fields/Ferry.java
@@ -3,6 +3,11 @@ package Entity.Fields;
 import java.awt.*;
 public class Ferry extends Ownable {
 
+    /**
+     * ferryRentFactor is the factor which the rent is multiple with for each owend ferry.
+     */
+    private int ferryRentFactor = 2;
+
     public Ferry(String fieldName, int propertyPrice, int propertyRent, int propertyPledgePrice, int ownerID, Color propertyColor) {
         super(fieldName, propertyPrice, propertyRent, propertyPledgePrice, ownerID, propertyColor);
     }
