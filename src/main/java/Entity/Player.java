d5feebaa3bbbc3a08c7d934112941fd7f6b61039
Karl Emil <kehansen3@hotmail.dk>
2020-01-14 14:35:07 +0100

    Tilføjede konstruktør i klassen PlayerList.

--- src/main/java/Entity/Player.java
+++ src/main/java/Entity/Player.java
@@ -80,3 +80,4 @@ public class Player {
     public String toString() { return name; }
 
 }
+
--- src/main/java/Entity/PlayerList.java
+++ src/main/java/Entity/PlayerList.java
@@ -1,20 +1,21 @@
 package Entity;
 
+import java.awt.*;
+
 public class PlayerList {
-    private static  final PlayerList INSTANCE = new PlayerList();
+    private static final PlayerList INSTANCE = new PlayerList();
 
-    private String players[];
+    private Player[] players;
     private int playerNumber;
+    private Color[] colorList = {Color.BLACK, Color.YELLOW, Color.BLUE, Color.GREEN, Color.RED, Color.MAGENTA};
+    public String [] playerNames = {"Bo i folkswagen","Lis i Up", "Jens i goCar", "Brian i BMW","Britta Jensen", "Olga på cykel"};
 
-    public int getPlayerNumber(int number) {
-        int playerNumber;
-        for(int i = 0; i < players.length; i++) {
-            playerNumber = Integer.parseInt(players[i]);
-            if(playerNumber == number) {
-                this.playerNumber = playerNumber;
-            }
+    public PlayerList(int playersNum) {
+        this.playerNumber = playersNum;
+        players = new Player[playersNum];
+        for (int i = 0;i < playersNum;i++) {
+            players[i] = new Player(null, colorList[i]);
         }
-        return this.playerNumber;
     }
 
     public static PlayerList getINSTANCE(){
