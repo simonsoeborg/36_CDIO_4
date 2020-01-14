eb95662817125692c39724b24168150f58e195db
SImon <fridolf.lindhard@gmail.com>
2020-01-14 18:16:15 +0100

    Udvid konstruktør

--- src/main/java/Entity/Fields/Field.java
+++ src/main/java/Entity/Fields/Field.java
@@ -1,12 +1,18 @@
 package Entity.Fields;
 
+import java.awt.*;
+
 public class Field {
 
     private String fieldName;
 
-    public Field(String fieldName) {
+    private Color color;
+
+    public Field(String fieldName,Color color) {
         this.fieldName = fieldName;
+        this.color = color;
     }
+
 
     public String getFieldName() {
         return fieldName;
