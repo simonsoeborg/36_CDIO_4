c8a1276f066e04ab86f5607415867f8e1adb25b8
Freja <frejaj9@gmail.com>
2020-01-14 15:55:27 +0100

    propertyFields ændringer (not done)

--- src/main/java/Logic/PropertyFields.java
+++ src/main/java/Logic/PropertyFields.java
@@ -1,6 +1,19 @@
 package Logic;
 
+import Entity.Fields.Field;
+import Entity.Fields.Ownable;
+import Entity.Fields.Street;
+import Entity.GameBoard;
+import Entity.Player;
+
 public class PropertyFields {
 
-    
+    private static GameBoard gb = GameBoard.getInstance();
+    private Field[] fields = gb.getFields();
+
+    public int CheckFieldOwner(int fieldIndex, int currentPlayer){
+
+        int i = ((Ownable)(fields[fieldIndex]).getOwnerID);
+
+    }
 }
