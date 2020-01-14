e6e70a61763a6dbee887ca5c5280dfb146b88839
Karl Emil <kehansen3@hotmail.dk>
2020-01-14 13:54:14 +0100

    Tilføjede de tidligere lavede metoder i klassen Player.

--- src/main/java/Entity/Player.java
+++ src/main/java/Entity/Player.java
@@ -3,33 +3,80 @@ package Entity;
 import java.awt.*;
 
 public class Player {
-    private String playerName;
+    private String name;
+    private Color color;
     private int money;
     private int fieldIndex;
     private boolean bankruptcy;
-    private boolean inPrison;
+    private boolean inJail;
+    private boolean freeOutOfJail;
+    private boolean buying;
 
-    public String getPlayerName() {
-        return playerName;
+    public Player(String name, Color color) {
+
+        this.name = name;
+        this.color = color;
+        money=0;
+        fieldIndex = 0;
+        bankruptcy =false;
+        inJail =false;
+        freeOutOfJail=false;
+        buying=false;
     }
 
-    public void setPlayerName(String Name) {
-        playerName = Name;
+    public String getName() { return name; }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
     }
 
     public int getMoney() {
+
         return money;
     }
 
-    public void setMoney(int amount) {
-        money = amount;
+    public void setMoney(int money) {
+        this.money = money;
     }
 
-    public boolean setBankrupt(int playerNumber) {
-        return bankruptcy;
+    public void addMoney(int diff) {
+        if (money + diff >= 0) {
+            money = money + diff;
+        } else {
+            setBankruptcy(true);
+            money = money + diff;
+        }
     }
 
-    public boolean isPlayerInPrison(int playerNumber) {
-        return inPrison;
-    }
+    public boolean getBankruptcy() {return bankruptcy;}
+
+    public void setBankruptcy(boolean bankruptcy) { this.bankruptcy = bankruptcy; }
+
+    public int getFieldIndex() { return fieldIndex; }
+
+    public void setFieldIndex(int fieldIndex) { this.fieldIndex = fieldIndex; }
+
+    public boolean getInJail(){return inJail;}
+
+    public void setInJail(boolean inJail) { this.inJail = inJail; }
+
+    public boolean getFreeOutOfJail() { return freeOutOfJail; }
+
+    public void setFreeOutOfJail(boolean freeOutOfJail) { this.freeOutOfJail = freeOutOfJail; }
+
+    public boolean getBuying() {return buying; }
+
+    public void setBuying(boolean buying) { this.buying = buying; }
+
+    @Override
+    public String toString() { return name; }
+
 }
