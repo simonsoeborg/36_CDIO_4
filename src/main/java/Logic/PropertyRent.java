ae1fcb6988e0ce36927a31c258c2793ab061fbc8
Karl Emil <kehansen3@hotmail.dk>
2020-01-15 11:23:13 +0100

    payRent er done

--- src/main/java/Logic/PropertyRent.java
+++ src/main/java/Logic/PropertyRent.java
@@ -3,9 +3,9 @@ package Logic;
 import Entity.Fields.Field;
 import Entity.Fields.Ownable;
 import Entity.Fields.Street;
+import Entity.FileReader;
 import Entity.GameBoard;
 import Entity.Player;
-
 /**
  * handles everthing with rent
  * @author David, Kristoffer
@@ -14,6 +14,7 @@ import Entity.Player;
 public class PropertyRent {
 
     private GameBoard gameBoard;
+    private FileReader reader = new FileReader();
 
     public PropertyRent(GameBoard gameBoard) {
         this.gameBoard = gameBoard;
@@ -22,27 +23,30 @@ public class PropertyRent {
      * payRent transfer the money from the player to the owner.
      * first it checks all the fields on the gameboard to see if the owner owns all fields of same color
      * Which result in double rent
-     * @param owner - the owner of the field
-     * @param p - the player who
-     * @param propertyField
+     * @param ownerID - the ownerID of the player who owns the field
+     * @param turn - turn of the current player
+     * @param pl - the player list
+     * @param fl - the field list
      */
-    public void payRent(Player owner, Player p, Street propertyField){
+    public void payRent(int ownerID, int turn, Player[] pl, Field[] fl){
 
-        // vi bruger en foreach til at gennemløbe alle elementer i fieldArry
+        // vi bruger et for-loop til at gennemløbe alle elementer i feltlisten
         // hvis et felt har samme farve som propertyField sættes count 1 op
+        Player p = pl[turn];
         int count = 0;
         int rentDouble = 1;
-        for (Field field:gameBoard.getFields()){
-
-            /// TODO: 14-01-2020 tilføj GUIField farverne
-            //if ( det ene felt farve == andetfeltes farve)
-
-            count++;
+        int actualColor = Integer.parseInt(reader.read(4, p.getFieldIndex()));
+        int sameColor;
+        for (int i = 1; i < fl.length+1; i++) {
+            sameColor = Integer.parseInt(reader.read(4, i));
+            if (actualColor == sameColor)
+                if (ownerID == ((Ownable)fl[i]).getOwnerID())
+                    count++;
         }
 
         // her tjekker vi om navnet for propertyField er en af de felter hvor der kun er 2 af samme farve
         // hvis ikke skal der være 3.
-        switch (propertyField.getFieldName()){
+        switch (fl[p.getFieldIndex()].getFieldName()){
 
             case "Rødovrevej": case "Hvidovrevej":
             case "Frederiks- \\nberggade": case"Rådhuspladsen":
@@ -52,7 +56,7 @@ public class PropertyRent {
             break;
         }
 
-        p.addMoney(-(propertyField.getPropertyRent()*rentDouble));
-        owner.addMoney(propertyField.getPropertyRent()*rentDouble);
+        p.addMoney(-(((Street)fl[p.getFieldIndex()]).getPropertyRent()*rentDouble));
+        pl[ownerID-1].addMoney(((Street)fl[p.getFieldIndex()]).getPropertyRent()*rentDouble);
     }
 }
