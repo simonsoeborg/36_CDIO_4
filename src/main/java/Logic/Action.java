03e76c1c3393982ec10d66cb46021c94be453ee3
Kristoffer <s180500@student.dtu.dk>
2020-01-18 15:56:21 +0100

    Action arbejdes stadig på

--- src/main/java/Logic/Action.java
+++ src/main/java/Logic/Action.java
@@ -6,6 +6,11 @@ import Entity.GameBoard;
 import Entity.Player;
 import Entity.PlayerList;
 
+/**
+ * This is a class, for all possible decisions in the game,
+ * @author KE
+ * @1.0.0
+ */
 public class Action {
     private LogicList ll = new LogicList();
     private DiceCup dc =  DiceCup.getINSTANCE();
@@ -15,7 +20,7 @@ public class Action {
     private Field[] fl = gb.getFields();
     private PlayerList pl = PlayerList.getInstance();
     private Taxes taxes = new Taxes();
-    private PropertyRent pr = new PropertyRent(gb);
+    private PropertyRent pr = new PropertyRent();
     private PropertyFields pf = new PropertyFields();
     private JailLogic jl = new JailLogic();
     private ChanceCardLogic cl = ChanceCardLogic.getInstance();
@@ -25,16 +30,26 @@ public class Action {
     private static final Action INSTANCE = new Action();
 
 
-
+    /**
+     * So on the button in game is a String. When you click on it, it copy that String on the botton
+     * and then run the swicth case for that specific String.
+     * @param action - action is a String
+     * @param p - the Current player whose playing
+     * @return
+     */
     public String decideAction (String action, Player p) {
 
         option = "";
 
         switch (action) {
 
+
+ //----------------------------------------------Roll Dice------------------------------------------------------------//
+            // rul terning for at bevæge spilleren.
             case "Rul":
                 dc.roll(); //Test
 
+                // hvis spilleren slår double med terningen.
                 if (jl.checkNumDoubles(p, dc.isFaceValueSame()))
                     option = "3Doubles";
                 else {
@@ -43,6 +58,8 @@ public class Action {
                 }
                 break;
 
+ //----------------------------------------------Land on a field------------------------------------------------------//
+            // vil du købe feltet
             case "Køb":
                 po.buyField(p, fl);
                 checkForExtra(p);
@@ -50,26 +67,32 @@ public class Action {
 
             case "Køb ikke":
             case "Ok":
+
+                // under at bygge huse
             case "Fortryd":
             case " ":
                 checkForExtra(p);
                 break;
 
+                // extradordinær skat
             case "Betal 2.000kr.-":
                 taxes.stateTax(p);
                 checkForExtra(p);
                 break;
 
+                // inkomstaskat
             case "Betal 4.000kr.-":
                 taxes.incomeTaxCash(p);
                 checkForExtra(p);
                 break;
 
+                // -||-
             case "Betal 10%":
                 taxes.incomeTaxPercentage(p);
                 checkForExtra(p);
                 break;
 
+                // betal for renten af en spillers felt
             case "Betal":
                 payRent(p);
                 checkForExtra(p);
@@ -98,6 +121,7 @@ public class Action {
                 break;
 
 //----------------------------------------------House-----------------------------------------------------------------//
+// farverne på felterne og hvor mange Streets der så af på hver af farverne
 
             case "Byg hus":
                 option = "BuildHouse";
@@ -217,11 +241,11 @@ public class Action {
     }
 
 
-
-
-
-
-
+    /**
+     * A method to make sure the player, can do a thing multiable times, and have a choice afterward.
+     * Such as they can buy Houses X amount and after they have bought it, they can continue or end turn.
+     * @param p - the current players turn
+     */
     private void checkForExtra(Player p) {
         if (po.canBuildHouse(fl, p))
             option += "House";
@@ -233,6 +257,11 @@ public class Action {
             option += "End";
     }
 
+    /**
+     * If the field is owned, the current player needs to pay. This method makes sure to let know what kind of thing
+     * the player needs to pay for. (street, ferry, brewery)
+     * @param p - the current player
+     */
     private void payRent(Player p) {
         int owner = pf.whoseProberty(p.getFieldIndex());
         if (fl[p.getFieldIndex()] instanceof Street)
@@ -245,11 +274,20 @@ public class Action {
             pr.payRentBrewery(owner, p, pl.getPlayers(), fl);
     }
 
+    /**
+     * a counter for the 3 times a player can be in jail
+     * @param p - current player in jail
+     */
     public void jailCountUp(Player p) {
         if (p.isInJail())
             p.addRoundInJail();
     }
 
+    /**
+     *  After 3 rounds in jail, where the player haven't rolled a double. they SHALL pay 1.000,- and then they are free
+     * @param p - current player who's about to get out
+     * @return
+     */
     public boolean threeRoundsInJail(Player p) {
         if (p.getRoundsInJail() == 3) {
             p.addMoney(-1000);
@@ -260,8 +298,11 @@ public class Action {
     }
 
 
-
-
+    /**
+     * When the player goes bankrupcty 
+     * @param gb
+     * @param p
+     */
     private void removePlayerFields(GameBoard gb, Player p){
         // Gennemløber alle felter og setter de felter der har samme id som spilleren til 0, så han ikke ejer dem mere.
         for (Field field: gb.getFields()) {
