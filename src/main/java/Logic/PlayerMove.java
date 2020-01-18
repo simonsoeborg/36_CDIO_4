9d6f540852ab84d5bc369a97dd036f028b310393
Kristoffer <s180500@student.dtu.dk>
2020-01-18 14:43:23 +0100

    move player clean up

--- src/main/java/Logic/PlayerMove.java
+++ src/main/java/Logic/PlayerMove.java
@@ -28,12 +28,18 @@ public class PlayerMove {
             p.addMoney(4000);
     }
 
+    /**
+     *
+     * @param p - the player
+     * @param toFieldNumber - Amount of fields the player has to move after have drawn a chanceCard there moves the player
+     */
     public void movePlayerChanceCard(Player p, int toFieldNumber) {
 
         int previous = p.getFieldIndex();
         p.setFieldIndex(toFieldNumber);
         int actual = p.getFieldIndex();
 
+        // hvis de passere start får de så start beløbet.
         if (actual < previous)
             p.addMoney(4000);
     }
